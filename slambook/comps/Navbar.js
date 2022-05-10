00e81864fa920b7dbdb6bc9f5ee6c8975e95734a
VaibhavMathur-2003 <mathurvaibhav010@gmail.com>
2022-05-10 23:53:35 +0530

    DetailUpdate

--- slambook/comps/Navbar.js
+++ slambook/comps/Navbar.js
@@ -2,39 +2,56 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+  const logoCSS = {
+    marginLeft: "1vh",
+    // marginRight: "30vw",
+    marginTop: "2vh",
+    cursor: "pointer",
+  };
 
-    const logoCSS = {
-        marginLeft: "1vh",
-        marginRight: '30vw',
-        marginTop: "2vh",
-        cursor: 'pointer'
-      }
+  const linkCSS = {
+    marginLeft: "4vw",
+    fontSize: "21px",
+    color: "white",
+    textShadow: "0px 3px 5px black",
+    cursor: "pointer",
+    fontWeight: "500",
+    letterSpacing: "2px",
+  };
 
-      const linkCSS = {
-        marginLeft: '4vw',
-        fontSize: '21px',
-        color: 'white',
-        textShadow: '0px 3px 5px black',
-        cursor: 'pointer',
-        fontWeight: '500',
-        letterSpacing: '2px'
-      }
+  const parentLink = {
+    display: "flex",
+    marginTop: "5vh",
+    width: "85%",
+    justifyContent: "center",
+  };
 
-      const parentLink = {
-          display: 'flex',
-          marginTop: '5vh'
-      }
-    
-    return ( 
-        <div style={{display: 'flex', backgroundColor: 'rgb(174, 229, 248)'}}>
-            <Link href="https://iitgoa.ac.in/"><div style={logoCSS}><Image src="/iitgoa_logo.png" width={80} height={80} /></div></Link>
-            <div  style={parentLink}>
-                <Link href="/branch"><div className="links" style={linkCSS}>BRANCH</div></Link>
-                <Link href="/"><div className="links" style={linkCSS}>SIGN IN</div></Link>
-                <Link href="/credit"><div className="links" style={linkCSS}>CREDITS</div></Link>
-            </div>
+  return (
+    <div style={{ display: "flex", backgroundColor: "rgb(174, 229, 248)" }}>
+      <Link href="https://iitgoa.ac.in/">
+        <div style={logoCSS}>
+          <Image src="/iitgoa_logo.png" width={80} height={80} />
         </div>
-     );
-}
- 
+      </Link>
+      <div style={parentLink}>
+        <Link href="/branch">
+          <div className="links" style={linkCSS}>
+            BRANCH
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="links" style={linkCSS}>
+            SIGN IN
+          </div>
+        </Link>
+        <Link href="/credit">
+          <div className="links" style={linkCSS}>
+            CREDITS
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
